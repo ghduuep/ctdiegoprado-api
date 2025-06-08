f921ac7b2529a1c577f3b0cfe2e4bcc600b63ab3
Guilherme Lima <ghduep@outlook.com>
2025-06-08 16:07:33 -0300

    adding student name and plan name at subscription serializer

--- subscriptions/serializers.py
+++ subscriptions/serializers.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 from .models import Subscription
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    student_name = serializers.SerializerMethodField()
+    plan_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Subscription
-        fields = '__all__'
+        fields = ['id', 'student', 'plan', 'start_date', 'end_date', 'status', 'student_name', 'plan_name']
+    
+    def get_student_name(self, obj):
+        return f"{obj.student.name} {obj.student.last_name}"
+
+    def get_plan_name(self, obj):
+        return obj.plan.name
 
