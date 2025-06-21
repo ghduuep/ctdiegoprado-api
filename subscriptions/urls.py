c6d13785a77a227bfeea03b25c79679922da4a5c
Guilherme Lima <ghduep@outlook.com>
2025-06-21 14:15:24 -0300

    creating dashboard stats view

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import SubscriptionViewSet
+from .views import SubscriptionViewSet, DashboardStatusView
 
 router = DefaultRouter()
 router.register(r'subscriptions', SubscriptionViewSet, basename='subscriptions')
+router.register(r'dashboard-status', DashboardStatusView, basename='dashboard-status')
 urlpatterns = router.urls
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,13 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from .models import Student
+from django.db.models import Count, Q
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Subscription
 from .serializers import SubscriptionSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-
-
-
 
 class SubscriptionViewSet(viewsets.ModelViewSet):
     queryset = Subscription.objects.select_related('student', 'plan').all()
@@ -19,3 +21,23 @@ class SubscriptionViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+class DashboardStatusView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        total_clientes = Student.objects.count()
+
+        subscription_stats = Subscription.objects.aggregate(
+            active = Count('id', filter=Q(status='active')),
+            paused = Count('id', filter=Q(status='paused')),
+            expired = Count('id', filter=Q(status='expired'))
+        )
+
+        data = {
+            'totalClientes': total_clientes,
+            'mensalidadesAtivas': subscription_stats.get('ativas', 0),
+            'mensalidadesPausadas': subscription_stats.get('pausadas', 0),
+            'mensalidadesExpiradas': subscription_stats.get('vencidas', 0)
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
